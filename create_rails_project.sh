4b71b0444e741d61b087f7ef44438f8dab16fa7a
kano <kano@localhost>
2013-05-19 19:12:17 +0900

    railsプロジェクト作成スクリプトを追加

---
+++ create_rails_project.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+tmp_dir='/tmp/rails_tmp'
+echo ""
+
+if [ $# -ne 1 ];then
+	echo "USAGE: $0 project_path"
+	exit
+fi
+
+set -e
+
+rm -rf ${tmp_dir}
+mkdir ${tmp_dir}
+cd ${tmp_dir}
+cat > Gemfile << EOS
+source "http://rubygems.org"
+gem "rails", "3.2.13"
+EOS
+
+bundle install --path vendor/bundle 2> /dev/null > /dev/null
+bundle exec rails new "$1" 2> /dev/null > /dev/null
+rm -rf ${tmp_dir} 
+
+echo "Succeeded in creating rails project"
+echo "Please execute \"bundle install --path vendor/bundle\" on $1"
+
